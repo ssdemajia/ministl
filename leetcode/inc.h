b2ec56445815c1ea3d0cfe99438d6de740b1d93b
ss <787178638@qq.com>
2017-07-15 20:48:15 +0800

    include file

---
+++ leetcode/inc.h
@@ -0,0 +1,16 @@
+#ifndef _INC_H
+#define _INC_H
+#include <iostream>
+//#include <algorithm>
+#include <vector>
+//#include <map>
+//#include <stack>
+using namespace std;
+void displayVec(std::vector<int> v)
+{
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+#endif
