f5b6cbffd0cac2469a2f6c57cacc406646f83143
ss <787178638@qq.com>
2017-07-19 10:29:02 +0800

    leetcode 9 add commit

--- leetcode/palindromeNumber.cpp
+++ leetcode/palindromeNumber.cpp
@@ -2,19 +2,19 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0||x%10 == 0) return false;
+        if (x < 0||(x!=0&&x%10 == 0)) return false;//当x为0时是回文数，x为10^n次方时不是回文数
         int temp = x;
         int r=0;
         while(temp>r)
         {
-            cout << temp << " , " << r << endl;
             r*=10;
             r+=temp%10;
             temp/=10;
         }
-        cout << temp << " , " << r << endl;
-        if (temp == r||temp == r/10)
+
+        if (temp == r || temp == r/10)//如果是1111的形式则满足第一个条件，12321满足第二个条件
         {
+            return true;
         }
         else
         {
