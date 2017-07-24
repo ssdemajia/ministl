a6a57bad612ecfb000d788b2d945555d074fac0b
chashao <787178638@qq.com>
2017-07-24 10:06:26 +0800

    change inc.h,using template rewrite display()

--- leetcode/inc.h
+++ leetcode/inc.h
@@ -1,27 +1,29 @@
 #ifndef _INC_H
 #define _INC_H
 #include <iostream>
+#include <string>
 #include <algorithm>
 #include <vector>
 #include <climits>
 //#include <map>
 //#include <stack>
 using namespace std;
-void displayVec(std::vector<int> v)
+template <typename T>
+void displayVec(std::vector<T> v, string sep = " ")
 {
     for (size_t i = 0; i < v.size(); i++) {
-        cout << v[i] << " ";
+        cout << v[i] << sep;
     }
     cout << endl;
 }
 template <typename T>
-void displayVec2d(std::vector<std::vector<T> > v)
+void displayVec2d(std::vector<std::vector<T> > v,string sep = " ")
 {
     for (int i = 0; i < v.size(); i++)
     {
         for (int j = 0; j < v[0].size(); j++)
         {
-            cout << v[i][j] << " ";
+            cout << v[i][j] << sep;
         }
         cout << endl;
     }
