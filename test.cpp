0078dce724199ffccfa9a1a61493d90e8cf68722
ss <787178638@qq.com>
2017-08-09 21:35:47 +0800

    leetcode

--- test.cpp
+++ test.cpp
@@ -1,15 +1,26 @@
-#include "leetcode/inc.h"
+#include <iostream>
 
+
+
+
+template<typename T>
+struct plus {
+    T operator() (const T &x, const T& y) const {return x+y;}
+};
+// struct plus {
+//     int operator() (const int &x, const int& y) const {
+//         return x+y;
+//     }
+// };
+// template <class T>
+// struct minus {
+//     T operator() (const T &x, const T& y) const {return x-y;}
+// };
 int main(int argc, char const *argv[]) {
-    unordered_map<int,int>m;
-    m[1] = 1;
-    m[2] = 2;
-    m[3] = 3;
-    m[3] = 3-m[3];
-    for_each(m.cbegin(), m.cend(), [](const pair<int,int> &a){cout<<"["<<a.first<<"]:"<<a.second<< " ";});
-    cout << endl;
-    m.erase(3);
-    for_each(m.cbegin(), m.cend(), [](const pair<int,int> &a){cout<<"["<<a.first<<"]:"<<a.second<< " ";});
-    cout << endl;
+    //plus p;
+    plus<int> p;
+    //minus<int> m;
+    std::cout << p(1,2) << std::endl;
+    //cout << m(5,1) << endl;
     return 0;
 }
