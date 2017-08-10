b4044df736853cc0f6ddeda68b0788a4c15c13b5
ss <787178638@qq.com>
2017-08-10 19:08:00 +0800

    complete Leetcode 655

--- leetcode/PrintBinaryTree.cpp
+++ leetcode/PrintBinaryTree.cpp
@@ -1,50 +1,34 @@
 #include "inc.h"
 //leetcode 655
-struct TreeNode {
-   int val;
-   TreeNode *left;
-   TreeNode *right;
-   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+// struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+// };
 class Solution {
 public:
     vector<vector<string>> printTree(TreeNode* root) {
-        int depth = deep(root);
-        cout << "deep:"<<depth<<endl;
-        vector<vector<string> > result (depth, vector<string>());
-        std::cout << "/* message */" << '\n';
-        helper(root, result, depth);
+        int depth = deep(root);//求出当前树的深度
+        int width = pow(2, depth) - 1;//根据深度求出叶子的数量
+        vector<vector<string> > result (depth, vector<string>(width, ""));//构建二维数组
+        helper(root, result, 0, 0, width-1);
         return result;
     }
-    int helper(TreeNode* root, vector<vector<string> > &result, int deep) {
-        if (root->left == NULL && root->right == NULL) {
-            result[deep].push_back(to_string(root->val));
-            return 1;
+    void helper(TreeNode* root, vector<vector<string> > &result,int deep, int left, int right) {
+        if (root){
+            int index = (right-left)/2;//求出当前结点在这个深度的列
+            result[deep][index] = to_string(root->val);
+            if (root->left) {//左子树存在时递归遍历，深度加1,左边界不变，右边界变为中间
+                helper(root->left,result, deep+1, left, index-1);
+            }
+            if (root->right) {
+                helper(root->right, result, deep+1, index+1, right);
+            }
         }
-        int ls=0;
-        int rs=0;
-        if (root->right == NULL && root->left !=NULL) {
-            ls = helper(root->left, result, deep+1);
-            for (int i = 0; i < ls; i++) result[deep].push_back("");
-            result[deep].push_back(to_string(root->val));
-            for (int i = 0; i < ls; i++) result[deep].push_back("");
-            return ls*2+1;
-        }else if (root->right!=NULL && root->left == NULL) {
-            rs = helper(root->right, result, deep+1);
-            for (int i = 0; i < rs; i++) result[deep].push_back("");
-            result[deep].push_back(to_string(root->val));
-            for (int i = 0; i < ls; i++) result[deep].push_back("");
-            return rs*2+1;
-        }else {
-            ls = helper(root->left, result , deep+1);
-            for (int i = 0; i < ls; i++) result[deep].push_back("");
-            result[deep].push_back(to_string(root->val));
-            rs = helper(root->right, result, deep+1);
-            for (int i = 0; i < rs; i++) result[deep].push_back("");
-            return ls+rs+1;
-        }
+
     }
-    int deep(TreeNode *root) {
+    int deep(TreeNode *root) {//求树深度的方法
         if (!root) return 0;
         int ld = deep(root->left);
         int rd = deep(root->right);
