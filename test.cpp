297559e084bbc9dd225442f482f8b54a3f4e112f
chashao <787178638@qq.com>
2017-08-14 12:20:49 +0800

    Leetcode

--- test.cpp
+++ test.cpp
@@ -1,26 +1,8 @@
 #include <iostream>
+using namespace std;
 
-
-
-
-template<typename T>
-struct plus {
-    T operator() (const T &x, const T& y) const {return x+y;}
-};
-// struct plus {
-//     int operator() (const int &x, const int& y) const {
-//         return x+y;
-//     }
-// };
-// template <class T>
-// struct minus {
-//     T operator() (const T &x, const T& y) const {return x-y;}
-// };
 int main(int argc, char const *argv[]) {
-    //plus p;
-    plus<int> p;
-    //minus<int> m;
-    std::cout << p(1,2) << std::endl;
-    //cout << m(5,1) << endl;
+    int bit = 13;
+    cout << (((bit) + 9) & ~(9)) << endl;
     return 0;
 }
